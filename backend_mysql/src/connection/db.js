549cb9ebc9b44a86bee203d69092e13dfe89250a
Rodrigo de Andrade Lourenço <rodrigoalourenco@hotmail.com>
2026-04-07 18:44:06 -0300

    Update db.js
    GitGuardian has detected the following MySQL Assignment exposed within your GitHub account

--- backend_mysql/src/connection/db.js
+++ backend_mysql/src/connection/db.js
@@ -3,6 +3,6 @@ import sql from "mysql2"
 export const db = sql.createConnection({
     host: "sql3.freesqldatabase.com",
     user: "sql3822436",
-    password: "xVVVWJpSMr",
+    password: "",
     database: "sql3822436"
 })
