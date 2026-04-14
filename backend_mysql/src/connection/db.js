fbffff714d1975a6f27d61e837e5a24f5265f84f
Rodrigo de Andrade Lourenço <rodrigoalourenco@hotmail.com>
2026-04-13 22:12:20 -0300

    Update db.js

--- backend_mysql/src/connection/db.js
+++ backend_mysql/src/connection/db.js
@@ -3,6 +3,6 @@ import sql from "mysql2"
 export const db = sql.createConnection({
     host: "sql10.freesqldatabase.com",
     user: "sql10823122",
-    password: "6mdbeF2mE6",
+    password: "",
     database: "sql10823122"
 })
