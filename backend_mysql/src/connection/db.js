38f5656746546946e0374dd8a1e8a8a61e5fb7a6
Rodrigo Lourenço <rodrigo.lourenco@telefonica.com>
2026-04-13 22:18:36 -0300

    update routes e credential db

--- backend_mysql/src/connection/db.js
+++ backend_mysql/src/connection/db.js
@@ -1,8 +1,17 @@
 import sql from "mysql2"
 
 export const db = sql.createConnection({
-    host: "sql3.freesqldatabase.com",
-    user: "sql3822436",
+    host: "sql10.freesqldatabase.com",
+    user: "sql10823122",
     password: "",
-    database: "sql3822436"
+    database: "sql10823122"
+})
+
+db.connect((err) => {
+    if (err) {
+        console.error("❌ Erro ao conectar no banco:", err.message)
+        return
+    }
+
+    console.log("✅ Banco de dados conectado com sucesso!")
 })
